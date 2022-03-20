588c06fc469e1e1628b65abd59b11ebfd534af81
bfblog <68181011+bfblog@users.noreply.github.com>
2022-03-20 21:45:33 +0100

    added kubectl

--- docker/shell/install.sh
+++ docker/shell/install.sh
@@ -15,3 +15,9 @@ echo "installing argocd ..."
 VERSION=v2.3.1 # Select desired TAG from https://github.com/argoproj/argo-cd/releases
 curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
 chmod +x /usr/local/bin/argocd
+
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
