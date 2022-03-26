14eb62a6262d92f653f0ec923124d6b8c239af3c
bfblog <68181011+bfblog@users.noreply.github.com>
2022-03-26 21:28:38 +0100

    added developer shell

--- docker/shell/install.sh
+++ docker/shell/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 echo "updating packages ..."
+export DEBIAN_FRONTEND=noninteractive
+
 apt install -y gnupg curl
 
 echo "installing keptn CLI ..."
@@ -16,8 +18,21 @@ VERSION=v2.3.1 # Select desired TAG from https://github.com/argoproj/argo-cd/rel
 curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
 chmod +x /usr/local/bin/argocd
 
+echo "install kubectl ..."
 apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubectl
+
+echo "insstall docker ..."
+apt-get install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo "cleaning cache ..."
+apt-get clean
+ 
