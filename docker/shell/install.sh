3bea8a17b983ee310f7f6fcbb6cf53a4821114bc
bfblog <68181011+bfblog@users.noreply.github.com>
2022-03-27 20:12:11 +0200

    create home dir

--- docker/shell/install.sh
+++ docker/shell/install.sh
@@ -25,7 +25,7 @@ echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/s
 apt-get update
 apt-get install -y kubectl
 
-echo "insstall docker ..."
+echo "install docker ..."
 apt-get install -y ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
