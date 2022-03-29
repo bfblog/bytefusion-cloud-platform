86cb2189d3006a91b40ae7ba72de5d8ca2725fc4
bfblog <68181011+bfblog@users.noreply.github.com>
2022-03-29 23:19:51 +0200

    tektoncli v0.23.1

--- docker/shell/install.sh
+++ docker/shell/install.sh
@@ -8,10 +8,8 @@ echo "installing keptn CLI ..."
 curl -sL https://get.keptn.sh | bash
 
 echo "installing tektioncd CLI ..."
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3EFE0E0A2F2F60AA
-echo "deb http://ppa.launchpad.net/tektoncd/cli/ubuntu eoan main"| tee /etc/apt/sources.list.d/tektoncd-ubuntu-cli.list
-apt update
-apt install -y tektoncd-cli
+curl -o tkn.tar.gz -LO https://github.com/tektoncd/cli/releases/download/v0.23.1/tkn_0.23.1_Linux_x86_64.tar.gz
+tar xvzf tkn.tar.gz -C /usr/local/bin/ tkn
 
 echo "installing argocd ..."
 VERSION=v2.3.1 # Select desired TAG from https://github.com/argoproj/argo-cd/releases
