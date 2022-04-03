266def3a2dd647ac3743de23ae2c59a03c14d066
skippi1 <skippi1@gmx.de>
2022-04-03 13:12:37 +0200

    added tekton triggers v0.19.1

--- scripts/install-pipelines.sh
+++ scripts/install-pipelines.sh
@@ -1,3 +1,9 @@
-tkn pipeline start pipeline-with-parameters \
+#!/bin/bash
+
+BRANCH=$(git branch --show-current)
+
+tkn pipeline start bf-data-plf \
+  --namespace tekton-pipelines \
+  --prefix-name $BRANCH \
   --workspace name=source,claimName=bytefusion-cloud-platform-workspace \
   --showlog
