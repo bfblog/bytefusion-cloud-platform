94596a501a735fa3e4e23b592def64b9a88157f4
skippi1 <skippi1@gmx.de>
2022-04-17 19:48:55 +0200

    fix 2

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+actual_path=$(readlink -f "${BASH_SOURCE[0]}")
+script_dir=$(dirname "$actual_path")
+
 tkn pipeline start \
    --namespace tekton-pipelines \
    bytefusion-data-platform \
-   --workspace name=source,volumeClaimTemplateFile=./workspace-template.yaml
+   --workspace name=source,volumeClaimTemplateFile=$script_dir/workspace-template.yaml
